faf74f69baa813d9c7c70cda84347314d43780d8
eduardo rivas <r.eduardo92@gmail.com>
2020-06-15 21:46:25 -0500

    optimaze imgs

--- dist/app.c3f9f951.js
+++ dist/app.c3f9f951.js
@@ -339,7 +339,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59468" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60367" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
